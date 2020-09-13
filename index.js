b6b4dfe6833f1b3bfa8238ff0460edd9d09dd954
Reutov Dmitry <dimoff66@mail.ru>
2020-09-13 16:17:25 +0300

    fixed bugs, added tests

--- index.js
+++ index.js
@@ -1,54 +1,115 @@
-const screenedSymbols = '.{}[]^()+*?\\/$'.split('').reduce((map, s) => {
-  map[s] = '\\' + s
-  return map
-}, {})
-
-const defaultSettings = {
-  blocks: [],
-  mentions: [],
-  ignoreInsideQuotes: true, 
-  includeNeedleMode: 'NONE', 
-  ignoreCase: true, 
-  trimResult: true, 
-  trimNeedles: false,
-} 
 
 const prepareSearch = (needles, settings) => {
-  settings = {...defaultSettings, ...settings}
+  const defaultSettings = {
+    brackets: [],
+    mentions: [],
+    ignoreInsideQuotes: true, 
+    includeNeedleMode: 'NONE', 
+    ignoreCase: true, 
+    trimResult: true, 
+    trimNeedles: false,
+    check: undefined,
+    defaultBrackets: [['(', ')'], ['[', ']'], ['{', '}']]
+  } 
 
-  const { blocks, ignoreInsideQuotes, mentions } = settings
+  const splitSettings = {
+    ...defaultSettings, 
+    ...settings,
 
-  if (ignoreInsideQuotes) 
-    blocks.push([`'`, null, true], [`"`, null, true])
+    needles,
 
-  const blocksMap = blocks.reduce((map, [open, close, ignoreMode])=> {
-    map[open] = { open, ignoreMode, close: close || open }
-    return map
-  }, {}) 
+    init () {
+      if (Array.isArray(this.mentions) || typeof this.mentions === 'string') {
+        const mentions = [this.mentions].flat().filter(Boolean)
+        this.mentions = mentions.length > 0 && new Set(mentions)
+      }
+        
+      return this
+        .createBracketsMap()
+        .createBracketsSearch()
+        .createneedlesSearch()
+    },
 
-  const needlesArray = (typeof needles === 'string' || Array.isArray(needles)) && [needles].flat()
-  const blocksArray = blocks.flatMap(v => v.slice(0, 2)).concat(mentions).filter(Boolean)
+    merge (settings) {
+      if (!settings) return this 
 
-  let [needlesSearchPattern, blocksSearchPattern] = 
-    [needlesArray, blocksArray].map(arr => 
-      arr && arr.join('|').split('').map(s => screenedSymbols[s] || s).join('')
-    )
+      settings = {...this, ...settings }
+      if (['brackets', 'mentions'].some(prop => prop in settings)) {
+        settings.init()
+      }
 
-  const needlesRegExp = needlesSearchPattern ? new RegExp(needlesSearchPattern, 'g') : needles
-  const blocksRegExp = new RegExp(blocksSearchPattern, 'g')
+      return settings
+    },
 
-  settings.mentions = mentions.length > 0 && new Set(mentions)
+    arrayToPattern (arr) {
+      prepareSearch.screenedSymbols = 
+        prepareSearch.screenedSymbols || 
+        new Set('.{}[]^()+*?\\/$|'.split(''))
 
-  return { blocksMap, needlesRegExp, blocksRegExp, settings }
+      const { screenedSymbols } = prepareSearch
+      return arr
+        .map(string => 
+          string.split('').map(s => screenedSymbols.has(s) ? '\\' + s : s).join('')
+        )
+        .join('|')
+    },
+
+    createRegExp (pattern) {
+      return RegExp(pattern, 'g')
+    },
+
+    createBracketsMap () {
+      let { brackets = [], ignoreInsideQuotes } = this 
+      if (brackets === true) {
+        brackets = this.defaultBrackets
+      }
+      if (ignoreInsideQuotes) {
+        brackets.push([`'`, null, true], [`"`, null, true])
+      }
+
+      this.bracketsMap = brackets.reduce((map, [open, close, ignoreMode])=> {
+        map[open] = { open, ignoreMode, close: close || open }
+        return map
+      }, {}) 
+
+      return this
+    },
+
+    createBracketsSearch () {
+      const patternParts = Object.entries(this.bracketsMap)
+        .flatMap(([, { close, open }]) => close !== open ? [open, close] : open)
+        .concat(this.mentions ? [...this.mentions] : [])
+        .filter(Boolean)
+
+      const pattern = this.arrayToPattern(patternParts)
+      this.bracketsSearch = this.createRegExp(pattern)
+
+      return this
+    },
+
+    createneedlesSearch () {
+      const { needles } = this
+
+      if (typeof needles === 'string' || Array.isArray(needles)) {
+        const pattern = this.arrayToPattern([needles].flat().filter(Boolean))
+        this.needlesSearch = this.createRegExp(pattern)
+      } else {
+        this.needlesSearch = needles
+      }
+
+      return this
+    }
+  }
+
+  return splitSettings.init()
 }  
 
-const splitSmartly = (...args) => {
+const getSplitSmartlyArgs = (args, extraSettings) => {
   if (args.length === 2) {
-    if (typeof args[1] === 'string' || Array.isArray(args[1])) {
+    if (typeof args[1] === 'string' || Array.isArray(args[1])) 
       args.push({})
-    } else {
+    else 
       args.unshift(null)
-    }
   }
 
   else if (args.length < 2) 
@@ -57,82 +118,94 @@ const splitSmartly = (...args) => {
   else if (args.length > 3) 
     throw new Error('Too much arguments passed to splitSmartly function!!!')
 
-  let [string, needles, settings] = args 
+  if (extraSettings) args[2] = {...args[2], extraSettings}
 
-  const searchSettings = prepareSearch(needles, settings)
+  return args
+}
 
-  const splitFn = split.bind(searchSettings)
-  splitFn.getIterator = string => splitFn(string, [])
+const splitSmartly = (...args) => {
+  let [string, needles, settings] = getSplitSmartlyArgs(args)
+
+  const splitSettings = prepareSearch(needles, settings)
+
+  const splitFn = split.bind(splitSettings)
+  splitFn.getOne = (string, index, settings = {}) => {
+    if (isNaN(index)) 
+      throw new Error ('second parameter of `getOne` function should be index')
+    
+    return splitFn(string, { ...settings, indexes: index })
+  }
+  
+  splitFn.getFirst = (string, settings = {}) => {
+    return splitFn(string, { ...settings, indexes: 0 })
+  }
+
+  splitFn.getIndexes = (string, indexes, settings = {}) => {
+    if (!Array.isArray(indexes)) 
+      throw new Error ('second parameter of `getOne` function should be array of indexes')
+    
+    return splitFn(string, { ...settings, indexes })
+  }
+
+  splitFn.getIterator = (string, settings = {}) => {
+    return splitFn(string, { ...settings, returnIterator: true })
+  }
 
   return string !== null ? splitFn(string) : splitFn
 }
 
-function split (string, ...args) {
-  if (args.length && args.length === 1) {
-    if (typeof args[0] === 'number' || Array.isArray(args[0])) {
-      args.push({})
-    } else {
-      args.unshift(undefined)
-    }
-  }
-
-  const [indexes, settings] = args
-  const indexesParam = indexes !== undefined && [indexes].flat()
-
-  let res = new SearchResults(string, this, indexesParam, settings)
+function split (string, settings) {
+  const splitSettings = this.merge(settings)
+  let res = new SearchResults(string, splitSettings)
   
-  if (typeof indexes === 'number') 
+  if (typeof splitSettings.indexes === 'number') 
     res = res.getNext()
 
-  else if (!indexes) 
+  else if (!splitSettings.returnIterator) 
     res = res.getAll()
 
   return res
 } 
 
+
+
 class SearchResults {
-  constructor (...args) {
-    this.incomingArgs = args
+  constructor (string, searchSettings) {
+    Object.assign(this, { string, searchSettings })
     this.prepareSearch()
   }
 
   prepareSearch () {
-    const [string, searchSettings, indexes, incomingSettings] = this.incomingArgs
-    const { blocksMap, needlesRegExp: needlesSearch, blocksRegExp: blocksSearch, settings } = searchSettings
+    const { needlesSearch, bracketsSearch, indexes } = this.searchSettings
+    const indexesArr = [indexes].flat().filter(Boolean)
     
-    needlesSearch.lastIndex = blocksSearch.lastIndex = 0
+    Array.from([needlesSearch, bracketsSearch]).forEach(search => { search.lastIndex = 0 })
 
     Object.assign(this, { 
-      string, 
-      blocksMap, 
-      needlesSearch, 
-      blocksSearch, 
-      settings: { ...settings, ...incomingSettings },
-
-      blocks: [],
+      brackets: [],
       pipe: [],
       currentMentions: [],
       position: 0,
       isDone: false,
       freeArea: { start: 0, end: undefined },
       lastNeedle: undefined,
+      searchString: this.searchSettings.ignoreCase 
+        ? this.string.toUpperCase()
+        : string,
 
-      stringForSearch: settings.ignoreCase ? string.toUpperCase() : string,
-
-      indexes: indexes && (new class {
-        values = new Set(indexes)
-        max = Math.max(...indexes) 
-        
-        count = 0
+      indexes: indexesArr.length && {
+        values: new Set(indexesArr),
+        max: Math.max(...indexesArr), 
+        count: 0,
   
         hasIndex () { 
           return this.max === -Infinity || this.values.has(this.count++) 
-        }
+        },
   
         isOverMax () { 
           return this.max !== -Infinity && this.count > this.max 
         }
-      })
+      }
     })  
   }
 
@@ -140,34 +213,86 @@ class SearchResults {
     return this.pipe.length === 0
   }
 
-  checkNeedle (pNeedle) {
-    const { trimNeedles, trimResult, check } = this.settings 
+  getMentions (indexFrom, indexTo) {
+    const properMentions = [], restMentions = []
 
-    let [needle, needlePos] = 
+    for (const item of this.currentMentions) {
+      if (item.index >= indexFrom && item.index < indexTo) 
+        properMentions.push(item.mention)
+      else 
+        restMentions.push(item)
+    }
+   
+    return [properMentions.length && properMentions, restMentions]
+  }
+
+  trimResultText (text) {
+    return this.searchSettings.trimResult ? text.trim() : text
+  }
+
+  trimNeedleText (text) {
+    return this.searchSettings.trimNeedles ? text.trim() : text
+  }
+
+  checkNeedle (pNeedle) {
+    const { string } = this
+    const { check, includePositions, mentions } = this.searchSettings
+
+    let [needleText, needlePos] = 
       pNeedle 
         ? [pNeedle[0], pNeedle.index] 
-        : ['', this.string.length]
+        : ['', string.length]
 
-    let text = this.string.substring(this.position, needlePos)
+    let text = string.substring(this.position, needlePos)
 
-    if (!needle) this.isDone = true
+    if (!needleText) this.isDone = true
 
-    if (trimResult) text = text.trim()
-    if (trimNeedles) needle = needle.trim()
+    text = this.trimResultText(text)
+    needleText = this.trimNeedleText(needleText)
 
-    if (check && !check(text, needle)) return []
+    let needle = needleText
+    if (includePositions) {
+      text    = { text, position: this.position }
+      needle  = { text: needle, position: needlePos, isNeedle: true }
+    }
+
+    let restMentions
+    if (mentions) {
+      text = typeof text === 'string' ? { text } : text 
+      const [properMentions, restItems] = this.getMentions(this.position, needlePos)
+
+      if (properMentions) {
+        text.mentions = properMentions 
+        restMentions = restItems
+      }
+    }
+
+    if (check) {
+      const position = isNaN(this.tempPosition) ? this.position : this.tempPosition 
+      this.tempPosition = needlePos + needleText.length
+
+      const textBefore = this.trimResultText(string.substring(position, needlePos))
+      const textAfter = string.substring(needlePos + needleText.length)
+
+      const mentions = this.getMentions(position, needlePos)[0]
+
+      if (!check({ string: textBefore, needle, textAfter, mentions })) return []
+      delete this.tempPosition
+    } 
+
+    if (restMentions) this.currentMentions = restMentions
 
     this.position = needlePos + needle.length
-    return [text, needle, needlePos, true]
+    return [text, needle, true]
   }
 
   pushToPipe (value) {
     if (this.indexes) {
-      if (!this.indexes.hasIndex()) return
+      if (!this.indexes.hasIndex()) 
+        return
       
-      if (this.indexes.isOverMax()) {
+      if (this.indexes.isOverMax()) 
         this.isDone = true
-      }
     }
 
     this.pipe.push(value)
@@ -176,28 +301,10 @@ class SearchResults {
   addToPipe (pNeedle) {
     const { position } = this
 
-    let [text, needle, needlePos, checked] = this.checkNeedle(pNeedle)
+    let [text, needle, checked] = this.checkNeedle(pNeedle)
     if (!checked) return false
 
-    const { includeNeedleMode, includePositions, mentions } = this.settings
-
-    const textIsEmpty = !text
-
-    if (includePositions) {
-      text    = { text, position }
-      needle  = { text: needle, position: needlePos, isNeedle: true }
-    }
-
-    if (mentions) {
-      text = typeof text === 'string' ? { text } : text 
-      const items = this.currentMentions.filter(item => item.index < needlePos)
-      if (items.length) {
-        text.mentions = items.map(item => item.mention) 
-        this.currentMentions = this.currentMentions.filter(item => !items.includes(item))
-      }
-    }
-
-    switch (includeNeedleMode) { 
+    switch (this.searchSettings.includeNeedleMode) { 
       case INCLUDE_NEEDLE_SEPARATELY:
         this.pushToPipe(text)
         if (needle) 
@@ -209,6 +316,7 @@ class SearchResults {
         break 
 
       case INCLUDE_NEEDLE_RIGHT:
+        const textIsEmpty = !(typeof text === 'object' ? text.text : text)
         if (!textIsEmpty || this.lastNeedle)
           this.pushToPipe([ this.lastNeedle, text ])
         this.lastNeedle = needle
@@ -222,11 +330,11 @@ class SearchResults {
   }
 
   findFreeArea () {
-    const { needlesSearch, blocksSearch, blocks, blocksMap, 
-      freeArea, settings, stringForSearch: string } = this
+    const { searchString: string, brackets, freeArea, searchSettings } = this
+    const { bracketsSearch, needlesSearch } = searchSettings 
 
     while (!freeArea.end) {
-      const match = blocksSearch.exec(string)
+      const match = bracketsSearch.exec(string)
       if (!match) {
         if (isNaN(freeArea.start)) return false
 
@@ -235,51 +343,60 @@ class SearchResults {
       }
       
       const fragment = match[0]
-      const { close, ignoreMode } = blocks[blocks.length - 1] || {}
+      const { close, ignoreMode } = brackets[brackets.length - 1] || {}
 
       let block
       const ACTION_CLOSE = 1, ACTION_OPEN = 2, ACTION_ADD_FRAGMENT = 3
 
       const action = function getAction() {
-        if (fragment === close) return ACTION_CLOSE
-        if (!ignoreMode) {
-          if (block = blocksMap[fragment]) return ACTION_OPEN
-          if (settings.mentions && settings.mentions.has(fragment)) return ACTION_ADD_FRAGMENT
-        }
+        if (fragment === close) 
+          return ACTION_CLOSE
+
+        if (ignoreMode) return 
+
+        block = searchSettings.bracketsMap[fragment]
+        if (block) 
+          return ACTION_OPEN
+
+        if (searchSettings.mentions && searchSettings.mentions.has(fragment)) 
+          return ACTION_ADD_FRAGMENT
       } ()
 
       switch (action) {
         case ACTION_CLOSE:
-          if (--blocks.length === 0) {
-            freeArea.start = blocksSearch.lastIndex
-            if (needlesSearch && needlesSearch.lastIndex < freeArea.start) {
+          if (--brackets.length === 0) {
+            freeArea.start = bracketsSearch.lastIndex
+            if (needlesSearch && needlesSearch.lastIndex < freeArea.start) 
               needlesSearch.lastIndex = freeArea.start
-            }
           }  
           break 
 
         case ACTION_OPEN:
-          blocks.push(block)
-          if (blocks.length === 1) freeArea.end = match.index
+          brackets.push(block)
+          if (brackets.length === 1) 
+            freeArea.end = match.index
           break 
 
         case ACTION_ADD_FRAGMENT:
           this.currentMentions.push({ mention: fragment, index: match.index })
           break    
-      }
-      
+      }  
     }
 
     return true
   }
 
   findNeedle (needle) {
-    const { needlesSearch, freeArea, stringForSearch: string } = this
+    const { searchString: string, freeArea } = this
+    const { needlesSearch } = this.searchSettings
     
-    do {
+    let stopSearching
+    while (!stopSearching) {
       needle = needle || needlesSearch.exec(string)
-      if (!needle) this.addToPipe()
-      
+      if (!needle) {
+        this.addToPipe()
+      }
+
       else if (needle.index <= freeArea.end) {  
         const isAdded = 
           needle.index >= freeArea.start && 
@@ -290,11 +407,12 @@ class SearchResults {
         if (!isAdded) continue
       } 
       
-      else 
+      else {
         freeArea.start = freeArea.end = undefined
+      }
 
-      break
-    } while (true)
+      stopSearching = true
+    } 
 
     return needle
   }
@@ -302,13 +420,11 @@ class SearchResults {
   getNext () {
     let needle
     while (this.pipeIsEmpty && !this.isDone) {
-      if (!this.findFreeArea())
+      if (!this.findFreeArea()) 
         this.isDone = true
-      
-      else
-        needle = this.findNeedle(needle)
+      else 
+        needle = this.findNeedle(needle)  
     }  
-
     return this.pipeIsEmpty ? null : this.pipe.shift()
   }
 
@@ -317,7 +433,8 @@ class SearchResults {
   getRest () {
     const res = []
     let value
-    while (null !== (value = this.getNext())) res.push(value)
+    while (null !== (value = this.getNext())) 
+      res.push(value)
     return res
   }  
 
--- index.test.js
+++ index.test.js
@@ -5,13 +5,12 @@ const queryText = 'select love, joy from life where nobody and nothing'
 
 const queryNeedles = ['WHERE ', 'SELECT ', 'FROM ']
 
-const blocks = [['(', ')']]
-const splitQuery = splitSmartly(queryNeedles, { blocks })
+const splitQuery = splitSmartly(queryNeedles, { brackets: true })
 
 test('test add needles modes', () => {
 
   // MODE NONE
-  let res = splitSmartly(numbersText, ',', { blocks })
+  let res = splitSmartly(numbersText, ',', { brackets: true })
   expect(res).toEqual(['1', '2 (3, 4)', '"5,6"'])
 
   // MODE SEPARATELY
@@ -28,23 +27,63 @@ test('test add needles modes', () => {
 })
 
 test('test call with indexes', () => {
-  let res = splitQuery(queryText, [], { 
-    includeNeedleMode: INCLUDE_NEEDLE_SEPARATELY, 
-    trimNeedles: true, 
-    trimResult: false 
-  })
+  let res = splitQuery.getIterator(queryText, { includeNeedleMode: INCLUDE_NEEDLE_SEPARATELY })
+  
   expect(res.getNext()).toBe('')
-  expect(res.getNext()).toBe('SELECT')
-  expect(res.getNext()).toBe(' love, joy ')
+  expect(res.getNext()).toBe('SELECT ')
+  expect(res.getNext()).toBe('love, joy')
 
-  res = splitQuery(queryText, 2)
+  res = splitQuery.getOne(queryText, 2)
   expect(res).toBe('life')
 
-  res = splitQuery(queryText, [1, 3]).getAll()
+  res = splitQuery.getIndexes(queryText, [1, 3])
   expect(res).toEqual(['love, joy', 'nobody and nothing'])
 })
 
 test('test different settings props', () => {
-  let res = splitSmartly(queryNeedles, { blocks, mentions: ['LOVE']})(queryText, [1, 2]).getAll()
+  let res = splitQuery(queryText, { 
+    includeNeedleMode: INCLUDE_NEEDLE_SEPARATELY ,
+    trimNeedles: true,
+    trimResult: false,
+    indexes: [1, 2]
+  })
+  expect(res).toEqual(['SELECT', ' love, joy '])
+
+  // test mentions
+  res = splitSmartly
+    (queryNeedles, { brackets: true, mentions: ['LOVE']}).getIndexes (queryText, [1, 2])
   expect(res).toEqual([ { mentions: ["LOVE"], text: "love, joy" }, { text: 'life' } ])
+
+  res = splitSmartly('8 - 10 * -problems', '-', { 
+    check: ({ string, needle }) => needle !== '-' || !string.endsWith('*') 
+  })
+  expect(res).toEqual(['8', '10 * -problems'])
+
+  res = splitSmartly('life is long AND love BETWEEN pleasure AND pain', 'AND', { 
+    mentions: ' BETWEEN ',
+    check: ({ mentions, needle }) => !needle || !mentions
+  })
+  expect(res).toEqual([{ text: 'life is long'}, { text: 'love BETWEEN pleasure AND pain', mentions: [' BETWEEN ']}])
 })
+
+// const logIt = fn => {
+//   const exprText = fn.toString().split('\n').slice(1, -1).join('\n').substring(9).replace('Smart', 'Smartly')
+//   const res = JSON.stringify(fn()).replace(/\\"/g, '@').replace(/"/g, '\'').replace(/@/g, '"')
+//   console.log('res = ' + exprText + '\n// res: ' + res)
+// }
+
+// const splitSmart = splitSmartly
+// logIt(() => splitSmart('one / two / "three / four" / five / six', '/'))
+// logIt(() => splitSmart('(one / two) / "three / four" / five / six', '/', { brackets: true }))
+// logIt(() => splitSmart('(one / two) / "three / four" / <<five / six>>', '/', { brackets: [['(', ')'], ['<<', '>>']] }))
+// logIt(() => splitSmart('SELECT best FROM life', ['SELECT ', 'FROM ']))
+// logIt(() => splitSmart('SELECT best FROM life', ['SELECT ', 'FROM '], { includeNeedleMode: 'SEPARATELY' }))
+// logIt(() => splitSmart('SELECT best FROM life', ['SELECT ', 'FROM '], { includeNeedleMode: 'RIGHT' }))
+// logIt(() => splitSmart('select best from life', ['SELECT ', 'FROM '], { includeNeedleMode: 'RIGHT' }))
+// logIt(() => splitSmart('life is long AND love BETWEEN pleasure AND pain', 'AND', { 
+//   check: ({ string, needle }) => !needle || !string.toUpperCase().includes(' BETWEEN ')
+// }))
+// logIt(() => splitSmart('Peter loves Mary and Mary loves Johnny and Jonny loves Steve', 'AND', { 
+//   mentions: ['STEVE', 'PETER']
+// }))
+// logIt(() => splitSmart('One | Two | Three | Four', '|', { indexes: [1, 3] }))
